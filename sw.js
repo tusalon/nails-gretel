8fcaf3bf19d12b6bd8d4a04aab74b04775d764b8
TuDespensa25 <roma220130@gmail.com>
2026-05-30 09:17:39 -0400

    Remove floating install prompt

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker para Nails Gretel
 
-const CACHE_NAME = 'nails-gretel-v42';
+const CACHE_NAME = 'nails-gretel-v43';
 const urlsToCache = [
   '/nails-gretel/',
   '/nails-gretel/index.html',
